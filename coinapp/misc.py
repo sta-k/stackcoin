c958d0f0384f9469f5d87d089c3cdb862582b25e
suhailvs <suhailvs@gmail.com>
2024-12-30 21:00:51 +0530

    fixed initial datas

--- coinapp/misc.py
+++ coinapp/misc.py
@@ -273,3 +273,25 @@ COUNTRIES = [
     ("ZW", "Zimbabwe"),
     ("ZZ", "Unknown or unspecified country"),
 ]
+
+SAMPLE_OFFERINGS = [
+    ["Food_Drink", "rice", "Matta rice", "50$ per kg"],
+    [
+        "Food_Drink",
+        "coconut",
+        "Thenga. Coconut for chutney and curry.",
+        "20$ per piece",
+    ],
+    ["Food_Drink", "egg", "Egg recipies per piece", "8$ per piece"],
+    ["Food_Drink", "brinjal", "Vazhuthanangal, per kg", "20$ per kg"],
+    ["Food_Drink", "mulak_podi", "Mulaku podi per 100gram", "40$ per 100grams"],
+    ["Food_Drink", "salt", "uppu per kg", "20$ per kg"],
+    ["Electronics", "earphone", "Earphone", "800$ per piece"],
+    ["Electronics", "mp3_player", "MP3 Player for listening songs", "5000$ per piece"],
+    ["Electronics", "solarpanel", "Solar panel", "2000$ per piece"],
+    ["Clothing_Apparel", "underwear", "UnderWear", "200$ per piece"],
+    ["Clothing_Apparel", "tracksuit", "Track suit pants", "600$ per piece"],
+    ["Clothing_Apparel", "tshirt", "Tshirt", "300$ per piece"],
+    ["Accommodation_Space", "land", "25 meter/sq land", "50000$ per cent"],
+    ["Accommodation_Space", "room_rent", "Room rent/month", "5000$ per month"],
+]
